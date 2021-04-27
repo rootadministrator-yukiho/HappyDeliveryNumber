09deb7d0bef25f5e303c2cb5d54430e45e5e7272
Yukiho <3151598095@qq.com>
2021-04-27 18:43:49 +0800

    theFirst

---
+++ src/main/java/corsair/happydelivery/HappyDeliveryApplication.java
@@ -0,0 +1,13 @@
+package corsair.happydelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/corsair/happydelivery/happyDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package corsair.happydelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class happyDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
